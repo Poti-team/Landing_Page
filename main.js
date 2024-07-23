180c0fc345b5b6d702b5009c5eccc0da159e4b49
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2024-07-23 01:30:01 -0300

    corrigindo erro de index

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let iconModal = document.querySelector(".icon-modal");
 let botoes = document.querySelectorAll(".modal nav ul li a");
 let botaoDown = document.querySelector(".modal .btn-down");
 let todosDownload = document.querySelectorAll(".btn-down");
+let swiper = document.querySelector(".swiper")
 
 
 botoes.forEach(botao => {
@@ -22,9 +23,11 @@ function menuShow() {
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.body.style.overflow = "auto";
+        swiper.style.display = "block"
     } else {
         menuMobile.classList.add('open');
         document.body.style.overflow = "hidden";
+        swiper.style.display = "none"
     }
 }
 
